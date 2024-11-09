0ababee1df471ca373a4912b9cc00e07024e339e
HASHHman <SAHHH.ir@gmail.com>
2024-11-09 11:44:46 +0330

    Write drawGrid function

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const container = document.querySelector(".container");
 
-for (let x = 1; x <= 16; x++) {
-    for (let y = 1; y <= 16; y++) {
-        const div = document.createElement("div");
-        div.classList.add("cell");
-        div.textContent = y;
-        container.appendChild(div);
-        const width = div.offsetWidth;
-        div.style.height = width + "px";
+function drawGrid(width = 16) {
+    container.innerHTML = "";
+    for (let i = 0; i < width ** 2; i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        const containerWidth = container.offsetWidth;
+        const cellSize = Math.floor(containerWidth / width * 100) / 100;
+        cell.style.width = cellSize + "px";
+        cell.style.height = cellSize + "px";
+        cell.textContent = Math.floor(i / 16);
+        container.appendChild(cell);
     }
 }
+
 
 container.addEventListener('mouseover', (event) => {
     const div = event.target;
     div.classList.add("hovered");
 })
+
+drawGrid();
