8c84c4eda0596b9fa72a04454e53fa9c38a7ddc3
HASHHman <SAHHH.ir@gmail.com>
2024-11-10 09:25:59 +0330

    Add the progressive darkening effect

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const container = document.querySelector("#container");
 const sizeBtn = document.querySelector("#sizeBtn");
 
-function genRandomRGB() {
-	let randomRGB = "";
-	for (let i = 0; i < 3; i++) {
-		randomRGB = randomRGB + Math.floor(Math.random() * 225);
-		if (i < 2) {
-			randomRGB = randomRGB + ", ";
-		}
+function genRandomRGB(background) {
+	let RandomRGBValues = Array.from({ length: 3 }, () =>
+		Math.floor(Math.random() * 255)
+	);
+	let opacity = 0.1;
+	if (!!background && background.includes("a")) {
+		opacity = Number(background.match(/\d\.\d+/)) + 0.1;
+	} else if (!!background) {
+		opacity = 1;
 	}
-	return "rgb(" + randomRGB + ")";
+	return `rgba(${RandomRGBValues[0]} ${RandomRGBValues[1]} ${RandomRGBValues[2]} / ${opacity})`;
 }
 
 function drawGrid(width = 16) {
@@ -28,14 +30,14 @@ function drawGrid(width = 16) {
 
 container.addEventListener("mouseover", (event) => {
 	const div = event.target;
-	div.style.backgroundColor = genRandomRGB(div.style.backgroundColor);
+	div.style.background = genRandomRGB(div.style.background);
 });
 
 sizeBtn.addEventListener("click", () => {
 	let wantedSize = Number(
 		prompt("Please enter your preferred grid width(Max 50):", "16")
 	);
-	while ((wantedSize > 50) | (wantedSize == null)) {
+	while (wantedSize > 50 || wantedSize == null) {
 		wantedSize = Number(prompt("Please enter a value less than 50:", "16"));
 	}
 	drawGrid(wantedSize);
